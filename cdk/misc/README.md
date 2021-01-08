70e22866c3bcb4a35c5647c8b3aec79edd16e38b
Junxiang Ji <jxj@amazon.com>
2021-01-08 11:51:34 +0800

    WIP: data mock tool for reporting.

---
+++ cdk/misc/README.md
@@ -0,0 +1,6 @@
+This folder contains the tool to generate fake Order data so it can be visualized by Superset.
+
+
+See the following for more:
+
+> https://aws.amazon.com/cn/blogs/china/aws-teaches-you-to-play-with-apache-superset-to-visualize-the-data-in-amazon-s3/
--- cdk/misc/mock_reporting_data.py
+++ cdk/misc/mock_reporting_data.py
@@ -1,28 +1,75 @@
-from faker import Faker
-import random
 import datetime
-import hashlib
+import random
 # from province_city import language_code
-from multiprocessing import Process
 import string
-from faker.providers import python
-from faker.providers import date_time
-from faker.providers import address
-from pathlib import Path
 from datetime import timedelta
 from multiprocessing import Process
+from pathlib import Path
+
+from faker import Faker
+from faker.providers import address
+from faker.providers import date_time
+from faker.providers import python
 import gzip
+
+# pip install faker==4.0.1
+# https://faker.readthedocs.io/en/master/fakerclass.html
+#
+# Sample:
+#   {"Orders": [
+#         {
+#           "AmazonOrderId": "902-3159896-1390916",
+#           "PurchaseDate": "2017-01-20T19:49:35Z",
+#           "LastUpdateDate": "2017-01-20T19:49:35Z",
+#           "OrderStatus": "Pending",
+#           "FulfillmentChannel": "SellerFulfilled",
+#           "NumberOfItemsShipped": 0,
+#           "NumberOfItemsUnshipped": 0,
+#           "PaymentMethod": "Other",
+#           "PaymentMethodDetails": [
+#             "CreditCard",
+#             "GiftCerificate"
+#           ],
+#           "MarketplaceId": "ATVPDKIKX0DER",
+#           "ShipmentServiceLevelCategory": "Standard",
+#           "OrderType": "StandardOrder",
+#           "EarliestShipDate": "2017-01-20T19:51:16Z",
+#           "LatestShipDate": "2017-01-25T19:49:35Z",
+#           "IsBusinessOrder": false,
+#           "IsPrime": false,
+#           "IsGlobalExpressEnabled": false,
+#           "IsPremiumOrder": false,
+#           "IsSoldByAB": false
+#         }
+#       ]}
+
 
 file_dir = "fakeData/"
 separtor = '|'
 letters = string.ascii_letters
-row = '"app_id":"{}", "user_id":"{}", "channel_id":"{}", "server_id":"{}", "device_id":"{}", "role_name":"{}", ' \
-      '"level":"{}", ' \
-      '"vip":"{}", "trans_id":"{}", "payment_usd":"{}", "ip":"{}", "model":"{}", "resolution":"{}", "network":"{}", ' \
-      '"os":"{}", ' \
-      '"os_version":"{}", "provider":"{}", "event_time":"{}", "user_source":"{}", "game_version":"{}", "language":"{' \
-      '}", ' \
-      '"country":"{}", "event_type":"{}" '
+
+row = '"AmazonOrderId": "{}", ' \
+      '"PurchaseDate": "{}", ' \
+      '"LastUpdateDate": "{}", ' \
+      '"OrderStatus": "{}", ' \
+      '"FulfillmentChannel": "{}", ' \
+      '"NumberOfItemsShipped": {}, ' \
+      '"NumberOfItemsUnshipped": {}, ' \
+      '"PaymentMethod": "Other", ' \
+      '"PaymentMethodDetails": [ ' \
+      '"{}", ' \
+      '"{}" ' \
+      '], ' \
+      '"MarketplaceId": "{}", ' \
+      '"ShipmentServiceLevelCategory": "{}", ' \
+      '"OrderType": "StandardOrder", ' \
+      '"EarliestShipDate": "{}", ' \
+      '"LatestShipDate": "{}", ' \
+      '"IsBusinessOrder": false, ' \
+      '"IsPrime": {}, ' \
+      '"IsGlobalExpressEnabled": {}, ' \
+      '"IsPremiumOrder": {}, ' \
+      '"IsSoldByAB": {} '
 
 f = Faker()
 f.add_provider(python)
@@ -35,20 +82,17 @@ other_type = tuple("other_" + str(t) for t in range(95))
 types = ("login", "tutorial", "new_user", "payment") + other_type
 m = 1000000
 k = 1000
-uid_prefix = f.lexify(text='????????-????????-????????-',
-                      letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-channel_id_prefix = f.lexify(text='????????-????????-????????-',
-                             letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-device_id_prefix = f.lexify(text='????????-????????-????????-',
-                            letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-row_name_prefix = f.lexify(text='????????-????????-????????-',
-                           letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-model_prefix = f.lexify(text='????????-', letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-resolution_prefix = f.lexify(text='????????-', letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-network_prefix = f.lexify(text='????-', letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-os_elements = ("android", "ios")
-provider_elements = ("China_telecom", "China_unicom", "Other_1", "Other_2", "Other_3", "other_4")
-user_source_prefix = f.lexify(text='????-', letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+s = 128
+
+OrderStatus = ("Pending", "Paid", "Shipping")
+FulfillmentChannel = ("SellerFulfilled", "FBA")
+PaymentMethod = ("Discount", "GiftCerificate", "CreditCard")
+MarketplaceId = ("ATVPDKIKX0DER", "LASKDFUIYAUY", "ASHDFKJHWENRB")
+ShipmentServiceLevelCategory = ("Standard", "1-day", "15-day")
+OrderType = ("StandardOrder")
+
+#  --------- ref ------------
+
 language_code = ["af-ZA", "am-ET", "ar-AE", "ar-BH", "ar-DZ", "ar-EG", "ar-IQ", "ar-JO", "ar-KW", "ar-LB", "ar-LY",
                  "ar-MA", "arn-CL", "ar-OM", "ar-QA", "ar-SA", "ar-SY", "ar-TN", "ar-YE", "as-IN", "az-Cyrl-AZ",
                  "az-Latn-AZ", "ba-RU", "be-BY", "bg-BG", "bn-BD", "bn-IN", "bo-CN", "br-FR", "bs-Cyrl-BA",
@@ -73,42 +117,50 @@ language_code = ["af-ZA", "am-ET", "ar-AE", "ar-BH", "ar-DZ", "ar-EG", "ar-IQ", 
                  "uz-Latn-UZ", "vi-VN", "wo-SN", "xh-ZA", "yo-NG", "zh-CN", "zh-HK", "zh-MO", "zh-SG", "zh-TW", "zu-ZA"]
 
 
+def get_true_false():
+    if random.randint(0, 9) >= 5:
+        return "true"
+    else:
+        return "false"
+
+
+def get_random_date(from_y, from_m, from_d, to_y, to_m, to_d):
+    try:
+        date = f.date_time_between(start_date=datetime.datetime(from_y, from_m, from_d, 0, 0),
+                                   end_date=datetime.datetime(to_y, to_m, to_d, 0, 0),
+                                   tzinfo=None)
+        return date
+    except:
+        print(">>>>>> Skip Day {}-{}".format(to_m, to_d))
+        return "not valid date"
+
+
 def generate_one_json_row(from_y, from_m, from_d, to_y, to_m, to_d):
+    data_range = (from_y, from_m, from_d, to_y, to_m, to_d)
+
     r = row.format(
-        f.random_element(app_id_elements),
-        uid_prefix + str(random.randint(1, m)),
-        channel_id_prefix + str(random.randint(1, k)),
-        str(random.randint(1, k)),
-        device_id_prefix + str(random.randint(1, k)),
-        row_name_prefix + str(random.randint(1, k)),
-        str(random.randint(1, k)),  # level
-        str(random.randint(1, k)),  # VIP level
-        f.lexify(text='????????-????????-????????-???????',  # Trans_id
-                 letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-        str(random.randint(0, k)),  # payment usd
-        "{}.{}.{}.{}".format(str(random.randint(0, 255)), str(random.randint(1, 255)), str(random.randint(0, 255)),
-                             str(random.randint(0, 255))),
-        model_prefix + str(random.randint(0, k)),  # model
-        resolution_prefix + str(random.randint(0, 255)),
-        network_prefix + str(random.randint(0, 64)),
-        f.random_element(os_elements),
-        f.random_element(os_elements) + "{}.{}.{}".format(str(random.randint(4, 15)),
-                                                          str(random.randint(0, 5)),
-                                                          str(random.randint(0, 5))),
-        f.random_element(provider_elements),
-        f.date_time_between(start_date=datetime.datetime(from_y, from_m, from_d, 0, 0),
-                            end_date=datetime.datetime(to_y, to_m, to_d, 0, 0),
-                            tzinfo=None),
-        user_source_prefix + str(random.randint(0, 255)),
-        "{}.{}.{}".format(str(random.randint(4, 15)),
-                          str(random.randint(0, 5)),
-                          str(random.randint(0, 5))),
-        f.random_element(language_code),
-        f.country(),
-        f.random_element(types)
+        str(random.randint(1, 999)).zfill(3) + "-" + str(random.randint(1, m * 9)).zfill(7) + "-" + str(
+            random.randint(1, m * 9)).zfill(7),
+        get_random_date(*data_range),  # PurchaseDate
+        get_random_date(*data_range),
+        f.random_element(OrderStatus),
+        f.random_element(FulfillmentChannel),
+        random.randint(1, 5),  # Item shipped
+        random.randint(0, 1),  # Item not shipped
+        f.random_element(PaymentMethod),
+        f.random_element(PaymentMethod),  # May duplicate
+        f.random_element(MarketplaceId),
+        f.random_element(ShipmentServiceLevelCategory),
+        get_random_date(*data_range),
+        get_random_date(*data_range),
+        get_true_false(),  # is prime
+        get_true_false(),
+        get_true_false(),
+        get_true_false(),
+        # f.country(),
     )
 
-    return "{" + r + "}\n"
+    return "{" + r + "}" + "\n"
 
 
 def generate_path(from_y, from_m, from_d, to_y, to_m, to_d):
@@ -126,7 +178,7 @@ def generate_path(from_y, from_m, from_d, to_y, to_m, to_d):
         log_date = f_d + timedelta(days=d)
         partion_date = "date=" + log_date.strftime("%Y-%m-%d")
         # print(partion_date)
-        per_hour_file_names = [str(n).zfill(5) for n in range(240)]  # one day will have 240 files
+        per_hour_file_names = [str(n).zfill(5) for n in range(24)]  # one day will have 240 files
         for hour in per_hour_file_names:
             raw_path = "fakeData/adjust/raw_events/{}/".format(partion_date)
             raw_file_name = "raw_events_{}.gz".format(hour)
@@ -146,6 +198,24 @@ def process_one_pice(p, a_slice):
     print("================== a process is started to process {} files ==============".format(len(a_slice)))
     for i in a_slice:
 
+        # with gzip.open(i[0], "a", 9) as file:
+        with open(i[0], "w") as file:
+            print("process " + str(p) + ", write file: " + i[0] + ", for date " + str(i[1].day))
+            d = i[1]
+            for r in range(ROWS_A_FILE):
+                file.write(str(generate_one_json_row(d.year, d.month, d.day, d.year, d.month, d.day + 1)))
+    print("completed one.")
+
+
+def process_one_piece_gz(p, a_slice):
+    """
+    :param a_slice: A list of (path: date)
+    :return:
+    """
+    # a_slice = a_slice[0]
+    print("================== a process is started to process {} files ==============".format(len(a_slice)))
+    for i in a_slice:
+
         with gzip.open(i[0], "a", 9) as file:
             print("process " + str(p) + ", write file: " + i[0] + ", for date " + str(i[1].day))
             d = i[1]
@@ -154,7 +224,7 @@ def process_one_pice(p, a_slice):
     print("completed one.")
 
 
-ROWS_A_FILE = k  # k = 117KB, m = 116M, if set at m, then 1 day is 240*116M = 28G, 1 year is 10T
+ROWS_A_FILE = s  # s=128 k = 117KB, m = 116M, if set at m, then 1 day is 240*116M = 28G, 1 year is 10T
 
 if __name__ == "__main__":
     """
@@ -164,9 +234,9 @@ if __name__ == "__main__":
 
     """
     # Time span will decide how much log will be generated.
-    all_path = generate_path(2018, 1, 1, 2018, 3, 7)
+    all_path = generate_path(2019, 1, 1, 2019, 7, 2)
     # How many process to use.
-    num_process = 20
+    num_process = 8
     batch_size = len(all_path) // num_process
     remain_size = len(all_path) % num_process
     print("remain size " + str(remain_size))
@@ -175,9 +245,7 @@ if __name__ == "__main__":
         a_slice = all_path[crr:crr + batch_size]
         print("slice size " + str((crr, crr + batch_size)))
         crr = crr + batch_size
-        ps = Process(target=process_one_pice, args=[p, a_slice])
+        ps = Process(target=process_one_piece_gz, args=[p, a_slice])
         ps.start()
     if remain_size > 0:
-        Process(target=process_one_pice, args=[p, all_path[-remain_size]]).start()
-
-
+        Process(target=process_one_piece_gz, args=[p, all_path[-remain_size]]).start()
